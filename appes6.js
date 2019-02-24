375d0b22c02d7e8731398dc44aa57e5737b82236
Bridger <bridgerrhammond@gmail.com>
2019-02-24 14:44:49 -0600

    Added use of localStorage to persist bookList

--- appes6.js
+++ appes6.js
@@ -21,10 +21,15 @@ class UI {
     list.appendChild(row);
     // Event Listener - Delete
     document.querySelector(".delete").addEventListener("click", function(e) {
-      //   console.log(e);
       //   Instantiate UI
       const ui = new UI();
+      // Delete Book
       ui.deleteBook(e.target);
+      // Remove from LS
+      Store.removeBook(
+        e.target.parentElement.previousElementSibling.textContent
+      );
+
       // Show message
       ui.showAlert("Book Removed", "success");
 
@@ -61,6 +66,47 @@ class UI {
   }
 }
 
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function(book) {
+      const ui = new UI();
+      // add book to ui
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach(function(book, index) {
+      if ((book.isbn = isbn)) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+// DOM load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
 // Event Listener - Add
 document.getElementById("book-form").addEventListener("submit", function(e) {
   // Get form values
@@ -80,6 +126,8 @@ document.getElementById("book-form").addEventListener("submit", function(e) {
   } else {
     // Add book to list
     ui.addBookToList(book);
+    // add to LS
+    Store.addBook(book);
     // Show success
     ui.showAlert("Book added", "success");
 
